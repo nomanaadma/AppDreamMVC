a9f454cabaa52ab62da4c82bcf968592687a116f
Noman Shoukat <nomanaadma@gmail.com>
2020-04-01 15:13:45 +0500

    Deleting Posts

--- app/models/Post.php
+++ app/models/Post.php
@@ -61,4 +61,17 @@
 
       return $row;
     }
+
+    public function deletePost($id){
+      $this->db->query('DELETE FROM posts WHERE id = :id');
+      // Bind values
+      $this->db->bind(':id', $id);
+
+      // Execute
+      if($this->db->execute()){
+        return true;
+      } else {
+        return false;
+      }
+    }
   }
