ae7dc4e294163722c34c8a6233832e0080a5ad9f
Noman Shoukat <nomanaadma@gmail.com>
2020-04-01 15:02:17 +0500

    Post Model and Display

--- app/models/Post.php
+++ app/models/Post.php
@@ -7,9 +7,19 @@
     }
 
     public function getPosts(){
-      $this->db->query("SELECT * FROM posts");
+      $this->db->query('SELECT *,
+                        posts.id as postId,
+                        users.id as userId,
+                        posts.created_at as postCreated,
+                        users.created_at as userCreated
+                        FROM posts
+                        INNER JOIN users
+                        ON posts.user_id = users.id
+                        ORDER BY posts.created_at DESC
+                        ');
 
-      return $this->db->resultSet();
-      
+      $results = $this->db->resultSet();
+
+      return $results;
     }
   }
