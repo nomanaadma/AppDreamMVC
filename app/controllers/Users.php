c7c2fd324c9b15a0fe08fe46e88849549cf42757
Noman Shoukat <nomanaadma@gmail.com>
2020-04-01 15:00:35 +0500

    Posts Access Control

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -164,7 +164,7 @@
       $_SESSION['user_id'] = $user->id;
       $_SESSION['user_email'] = $user->email;
       $_SESSION['user_name'] = $user->name;
-      redirect('pages/index');
+      redirect('posts');
     }
 
     public function logout(){
@@ -173,13 +173,5 @@
       unset($_SESSION['user_name']);
       session_destroy();
       redirect('users/login');
-    }
-
-    public function isLoggedIn(){
-      if(isset($_SESSION['user_id'])){
-        return true;
-      } else {
-        return false;
-      }
     }
   }
